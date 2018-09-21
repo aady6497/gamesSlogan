008fc01497a5fa780539d43d36fd4ba336028ee0
Aditya Nandiraju <anandiraju@healthgrades.com>
2018-09-21 15:33:31 +0530

    Added a games App

--- games/views.py
+++ games/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Welcome to the Games Site:{}".format(datetime.now()))
